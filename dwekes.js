5cafe19292eeb09db222af203e838b579d0b0b72
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-12-05 16:38:46 +0900

    Add files via upload

---
+++ dwekes.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineageM/update/241120/bg_fishing.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
